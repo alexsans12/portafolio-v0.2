6a3d52a41ab78581fd0068e5cd9448b9d3b7bb6a
Alexsans <eddalexcab@gmail.com>
2020-06-18 21:16:03 -0600

    Se agrega la clase fixed al main-mobile__menu

--- js/main.js
+++ js/main.js
@@ -7,12 +7,14 @@ let menuOpen = false;
 mobileMenu.addEventListener('click', () => {
   if(!menuOpen) {
     mobileMenu.classList.add('main-mobile__menu--open');
+    mobileMenu.classList.add('fixed');
     mainMenu.setAttribute('style', 'right: 0; display: inline-block');
     main.classList.add('hidden');
     footer.classList.add('hidden');
     menuOpen = true;
   } else {
     mobileMenu.classList.remove('main-mobile__menu--open');
+    mobileMenu.classList.remove('fixed');
     mainMenu.setAttribute('style', 'right: 100vw');
     main.classList.remove('hidden');
     footer.classList.remove("hidden");
