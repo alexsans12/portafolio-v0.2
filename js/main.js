10b1083aa11968dd1b13826fb5eacdc871da8801
Alexsans <eddalexcab@gmail.com>
2020-06-18 22:22:21 -0600

    fix overflow en safari ios

--- js/main.js
+++ js/main.js
@@ -6,10 +6,12 @@ let menuOpen = false;
 mobileMenu.addEventListener('click', () => {
   if(!menuOpen) {
     mainMenu.classList.add('main-menu--open');
+    mobileMenu.classList.add('main-mobile__menu--open');
     body.classList.add('overflow');
     menuOpen = true;
   } else {
     mainMenu.classList.remove('main-menu--open');
+    mobileMenu.classList.remove('main-mobile__menu--open');
     body.classList.remove('overflow');
     menuOpen = false;
   }
